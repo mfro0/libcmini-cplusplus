20c7f39b120369f71818c8efe72275f79fc441ac
Markus Fröschle <mfro@mubf.de>
2019-01-29 11:00:18 +0100

    start definition of a sprite class

---
+++ sprite.cc
@@ -0,0 +1,32 @@
+#include <cstdint>
+#include "screen.h"
+
+using namespace screen;
+struct Sprite {
+    AtariScreen *screen;
+
+    uint16_t x;
+    uint16_t y;
+    uint16_t width;
+    uint16_t height;
+
+    uint8_t *sprite_data;
+    uint8_t *save_area;
+
+    Sprite(AtariScreen *sc) : screen(sc)
+    {
+
+    }
+
+    void save(void)
+    {
+
+    }
+
+    void draw(void) {
+        save();
+        for (int i = 0; i < height; i++)
+            ;
+            // memcpy(save_area, screen, )
+    }
+};
