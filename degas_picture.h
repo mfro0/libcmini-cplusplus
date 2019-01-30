d4913724d42e299de2df693b1ecc33d4cffe0004
Markus Fröschle <markus.froeschle@mahle.com>
2019-01-30 18:05:56 +0100

    fix alignment of picture_data member

--- degas_picture.h
+++ degas_picture.h
@@ -1,6 +1,8 @@
 #ifndef DEGAS_PICTURE
 #define DEGAS_PICTURE
 
+#include <cstddef>
+
 namespace {
     struct DegasPicture {
         uint16_t resolution;
@@ -9,10 +11,17 @@ namespace {
     };
 
     struct DegasPictureOverAllocated {
-        struct DegasPicture p;
         uint8_t filler[256];            // need to ensure we overallocate enough memory to align
-                                        // picture_data to an address evenly divisable by 256
+                                        // the picture_data member to an address evenly divisable by 256
                                         // (Atari ST hardware limitation)
+        DegasPicture p;
+
+        DegasPicture* fix_address(void)
+        {
+            return reinterpret_cast<DegasPicture *>((reinterpret_cast<uint32_t>(&p) & 0xffffff00) -
+                                                                               offsetof(DegasPicture, picture_data));
+                                                                               
+        }    
     };
 }
 #endif // DEGAS_PICTURE
