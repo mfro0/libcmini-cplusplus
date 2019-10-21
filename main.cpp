4a0206d4c152ed03cd6f4e5043b0b83bdb04a7a6
Markus Fröschle <markus.froeschle@mahle.com>
2019-10-21 08:55:19 +0200

    fix indentation (again)

--- main.cpp
+++ main.cpp
@@ -20,23 +20,21 @@ namespace {
         const TiledImage bees("images/bees.pi1", SpriteDefinition(40, 40));
         AtariGraphics::Image background("images/meadow.pi1");
 
-        {
-            AtariScreen screen(reinterpret_cast<uint32_t>(background.image_data()));
+        AtariScreen screen(reinterpret_cast<uint32_t>(background.image_data()));
             
-            std::array<Coord, 4> anodes = {{ { 10, 10 },
-                                            { 10, 11 },
-                                            { 11, 11 },
-                                            { 12, 11 } }};
-            // decltype(auto) anodes = std::make_array({10, 10}, {10, 11});
-            SpriteAnimation an(bees, AnimationPath(anodes));
-            
-            while (Cconis())
-                Cconin();
+        std::array<Coord, 4> anodes = {{ { 10, 10 },
+                                         { 10, 11 },
+                                         { 11, 11 },
+                                         { 12, 11 } }};
+        // decltype(auto) anodes = std::make_array({10, 10}, {10, 11});
+        SpriteAnimation an(bees, AnimationPath(anodes));
+          
+        while (Cconis())
+            Cconin();
 
-            (void) Cconws("press anykey"); (void) Cconin();
+        (void) Cconws("press anykey"); (void) Cconin();
 
-            screen.cleanup();
-        }
+        screen.cleanup();
     }
 }
 
